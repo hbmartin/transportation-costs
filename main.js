710158030d228c295f82d73c7685ee018c032433
Harold Martin <harold.martin@gmail.com>
2017-01-23 16:23:59 -0800

    * updated for new Google Maps UI * use GasBuddy instead of MapQuest for pricing

--- main.js
+++ main.js
@@ -86,17 +86,19 @@ var price = {
       return price.value( price.log[addr] );
     }
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://gasprices.mapquest.com/services/v1/stations?filter=gasprice%3Aregular&sortby=distance&location=" + addr + "&hits=1", true);
+    xhr.open("POST", "https://www.gasbuddy.com/Home/Search", true);
+	xhr.setRequestHeader('Content-type', 'application/json');
+	
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
         try {
-          price.value(JSON.parse(xhr.responseText).results[0].opisGasPrices[0].amount);
+          price.value(JSON.parse(xhr.responseText).stations[0].CheapestFuel.CreditPrice.Amount);
           price.log[addr] = price.value();
           driving_cost.render();
         } catch (e) { console.log(e); }
       }
     };
-    xhr.send();
+    xhr.send(JSON.stringify({s:addr}));
   }
 }
 
@@ -105,44 +107,33 @@ var driving_cost = {
     if (noround) { return price.value() * (1/mpg.value()) * driving_cost.distance; }
     else { return (price.value() * (1/mpg.value()) * driving_cost.distance).toFixed(2); }
   },
-  update: function(cards) {
-    var distance;
-    if (( distance=get_distance(cards) )) {
-      if (distance != driving_cost.distance) {
-        driving_cost.distance = distance;
-        try {
-          var addr = document.getElementsByClassName('tactile-searchbox-input')[0].value;
-          if (addr) {
-            price.update(addr);
-      }
-            var details = cards[0].getElementsByClassName('cards-directions-details-right');
-      var a;
-            for (var i=0; i<details.length; i++) {
-              if (details[i].style.display != 'none') {
-                if (details[i].innerText.indexOf('Parking') == -1) {
-                  a = document.createElement('a');
-                  a.style.margin = '0 15px 0 0';
-                  a.target = '_blank';
-                  a.innerText = 'Find Parking';
-                  details[i].insertBefore(a, details[i].children[0]);
-                }
-              }
-            }
-      var traffic_cards = document.getElementsByClassName('cards-traffic-title');
-      if (traffic_cards[0]) {
-        if (traffic_cards[0].innerText.indexOf('park') == -1) {
-          traffic_cards[0].innerHTML += ' (<a id="parkme" target="_blank" href="http://www.parkme.com/map#">parking</a>)';
+  update: function(distance) {
+    if (distance != driving_cost.distance) {
+      driving_cost.distance = distance;
+      try {
+        var addr = document.getElementsByClassName('tactile-searchbox-input')[0].value;
+        if (addr) {
+          price.update(addr);
         }
-        var searches = document.getElementsByClassName('tactile-searchbox-input');
-        if (searches.length > 1) {
-          document.getElementById('parkme').setAttribute("href", "http://www.parkme.com/map#" + searches[searches.length - 1].value);  
-                    a.href = 'http://www.parkme.com/map#' + searches[searches.length - 1].value;
+        var details = document.getElementsByClassName('last-waypoint')[0];
+        var destination = details.innerText;
+        var a;
+        if (details.style.display != 'none' && details.innerText.indexOf('Parking') == -1) {
+          a = document.createElement('a');
+          a.id = 'transportation-costs-parking'
+          a.target = '_blank';
+          a.class = 'line';
+          var h2 = document.createElement('h2');
+          h2.innerText = 'Find Parking';
+          a.appendChild(document.createElement('br'));
+          a.appendChild(h2);
+          details.appendChild(a);
+        } else {
+          a = document.getElementById('transportation-costs-parking');
         }
-      }
-          
-        } catch (e) { console.log(e); }
-        driving_cost.render();
-      }
+        a.href = 'http://www.parkme.com/map#' + destination;
+      } catch (e) { console.log(e); }
+      driving_cost.render();
     }
   },
   render: function() {
@@ -164,23 +155,52 @@ var cost = {
     return el;
   }(),
   update: function() {
-    var cards = document.getElementsByClassName('cards-directions');
-  	var cards_text = document.getElementById('cards').innerText;
-    if (!cards || cards.length == 0) {
-    	cost.el.style.display = 'none';
-    }
-    else if (cards_text.indexOf('Show traffic') != -1) {
-      driving_cost.update(cards);
-    }
-   	else if (cards_text.indexOf('Show terrain') != -1) {
-   		walk.update(cards);
-   	}
-   	else if (cards_text.indexOf('Show bike') != -1) {
-   		cycle.update(cards);
-   	}
-    else {
-      cost.el.style.display = 'none';
-    }
+  	//     var cards = document.getElementsByClassName('travel-mode');
+  	// var cards_text = document.getElementById('cards').innerText;
+  	//     if (!cards || cards.length == 0) {
+  	//     	cost.el.style.display = 'none';
+  	//     }
+  	//     else if (cards_text.indexOf('Show traffic') != -1) {
+  	//       driving_cost.update(cards);
+  	//     }
+  	//    	else if (cards_text.indexOf('Show terrain') != -1) {
+  	//    		walk.update(cards);
+  	//    	}
+  	//    	else if (cards_text.indexOf('Show bike') != -1) {
+  	//    		cycle.update(cards);
+  	//    	}
+  	//     else {
+  	//       cost.el.style.display = 'none';
+  	//     }
+	var distance, elements;
+	
+	elements = document.getElementsByClassName('section-trip-summary-subtitle');
+	if (elements && elements.length > 0) {
+		var re=/[\d\.]+/;
+		var matches = re.exec(elements[0].innerText);
+		if (matches && matches.length > 0) {
+			var _d = parseFloat(matches[0]);
+			if (_d && !isNaN(_d)) {
+				distance = _d;
+			}
+		}
+	}
+	
+	if (!distance) {
+		elements = document.getElementsByClassName('section-directions-trip-secondary-text');
+		if (elements && elements.length > 0) {
+			var _d = parseFloat(elements[0].innerText);
+			if (_d && !isNaN(_d)) {
+				distance = _d;
+			}
+		}
+	}
+	
+	if (!distance) {
+		cost.el.style.display = 'none';
+	} else {
+		driving_cost.update(distance);
+	}
   }
 };
 
